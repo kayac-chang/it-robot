aba6b6960b5925dfa3cde64e0ffda8d9fa881bc6
kayac-chang <egg734631@gmail.com>
2022-09-16 23:38:52 +0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,45 @@
+const puppeteer = require("puppeteer");
+
+function wait(ms) {
+  return new Promise((res) => setTimeout(res, ms));
+}
+
+async function main() {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto("https://ithelp.ithome.com.tw/users/20107239/ironman/5652");
+
+  let index = 0;
+  while (true) {
+    const links = await page.$$(".qa-list .qa-list__title a");
+
+    const link = links[index];
+
+    if (!link) break;
+
+    await Promise.all([
+      page.waitForNavigation(),
+      link.click(),
+      //
+    ]);
+
+    await wait(600);
+
+    await page.goBack();
+
+    index += 1;
+  }
+
+  const counts = await page.$$(
+    ".qa-list .qa-condition--change .qa-condition__count"
+  );
+  for (const count of counts) {
+    console.log(await count.evaluate((el) => el.textContent));
+  }
+
+  await browser.close();
+
+  console.log("automation process complete");
+}
+
+main();
